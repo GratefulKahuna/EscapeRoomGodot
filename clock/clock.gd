bb8934389bd41ddddee44a0f2dd5c13dfaf96e96
miguelvdw <s2988011@vuw.leidenuniv.nl>
2023-12-13 14:56:44 +0100

    clock aangepast correcte tijd is 17:10

--- clock/clock.gd
+++ clock/clock.gd
@@ -4,28 +4,42 @@ extends Control
 @onready var minuteangle = $clock/Minutes.rotation_degrees
 @onready var rotating_hour = false
 @onready var rotating_minute = false
+@onready var right_hour = false
+@onready var right_minute = false
+@onready var rotationhour = 3
+@onready var rotationminute = 9
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		if rotating_hour==true:
 			hourangle += 30
 			$clock/Hours.rotation_degrees = hourangle
+			rotationhour += 1
+			if (rotationhour % 12 == 2):
+				right_hour = true
+				print("right hour")
+			else:
+				right_hour = false
 		elif rotating_minute==true:
 			minuteangle += 30
 			$clock/Minutes.rotation_degrees = minuteangle
+			rotationminute += 1
+			if (rotationminute % 12 == 5):
+				right_minute = true
+				print("right minute")
+			else:
+				right_minute = false
+		if right_hour and right_minute:
+			print("correct time")
 
 func _on_controlminute_mouse_entered():
-	print("rotating_minute = true")
 	rotating_minute = true
 
 func _on_controlhour_mouse_entered():
-	print("rotating_hour = true")
 	rotating_hour = true
 
 func _on_controlminute_mouse_exited():
-	print("rotating_minute = false")
 	rotating_minute = false
 
 func _on_controlhour_mouse_exited():
-	print("rotating_hour = false")
 	rotating_hour = false
